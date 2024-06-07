6a83f8bdadf66561d2ae5ed3fa40bfb8001f342e
Elkin Torres <lalotor@yahoo.com>
2024-06-07 17:26:16 -0500

    JDBC lab instructions

--- lab/26-jdbc/src/main/java/config/RewardsConfig.java
+++ lab/26-jdbc/src/main/java/config/RewardsConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import rewards.RewardNetwork;
 import rewards.internal.RewardNetworkImpl;
 import rewards.internal.account.AccountRepository;
@@ -20,6 +21,11 @@ public class RewardsConfig {
 
 	@Autowired
 	DataSource dataSource;
+
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		return new JdbcTemplate(dataSource);
+	}
 		
 	@Bean
 	public RewardNetwork rewardNetwork(){
@@ -31,20 +37,17 @@ public class RewardsConfig {
 	
 	@Bean
 	public AccountRepository accountRepository(){
-		JdbcAccountRepository repository = new JdbcAccountRepository(dataSource);
-		return repository;
+    return new JdbcAccountRepository(jdbcTemplate());
 	}
 	
 	@Bean
 	public RestaurantRepository restaurantRepository(){
-		JdbcRestaurantRepository repository = new JdbcRestaurantRepository(dataSource);
-		return repository;
+    return new JdbcRestaurantRepository(jdbcTemplate());
 	}
 	
 	@Bean
 	public RewardRepository rewardRepository(){
-		JdbcRewardRepository repository = new JdbcRewardRepository(dataSource);
-		return repository;
+    return new JdbcRewardRepository(jdbcTemplate());
 	}
 	
 }
