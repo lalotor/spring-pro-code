1d272c43223c1442f33f48fc4366fb570d03079f
Elkin Torres <lalotor@yahoo.com>
2024-06-13 18:59:38 -0500

    Actuator lab instructions

--- lab/44-actuator/src/main/java/accounts/web/RestaurantHealthCheck.java
+++ lab/44-actuator/src/main/java/accounts/web/RestaurantHealthCheck.java
@@ -1,30 +1,30 @@
 package accounts.web;
 
-/**
- * TODO-16a: Create custom health indicator
- * - Make this class implement HealthIndicator interface
- * - Make this class a component
- * - Inject RestaurantRepository through constructor injection
- * - health() method should return DOWN if the repository is empty
- *   (no restaurants) or UP otherwise. (Note that RestaurantRepository
- *   has a method that returns number of restaurants.)
- */
-public class RestaurantHealthCheck {
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+import rewards.internal.restaurant.RestaurantRepository;
 
+@Component
+public class RestaurantHealthCheck implements HealthIndicator {
+
+  private RestaurantRepository restaurantRepository;
+
+  public RestaurantHealthCheck(RestaurantRepository restaurantRepository) {
+    this.restaurantRepository = restaurantRepository;
+  }
+
+  @Override
+  public Health health() {
+
+    Long count = restaurantRepository.getRestaurantCount();
+    if (restaurantRepository != null && count.compareTo(0L) > 0) {
+      return Health.up().build();
+    }
+
+    return Health
+        .status("NO_RESTAURANTS")
+        .withDetail("count", count)
+        .build();
+  }
 }
-
-
-/**
- * TODO-25 (Optional): Experiment with HealthIndicator above
- * - Change "spring.sql.init.data-locations" property in the
- *   "application.properties" file back to use "data-no-restaurants.sql"
- * - Include the restaurant count as extra detail when DOWN state.
- * - Instead of returning DOWN when there are no restaurants,
- *   define and use a custom status called NO_RESTAURANTS.
- * - When there are no restaurants in the DB, what top-level status
- *   is returned for the "application" health group?
- * - Set "management.endpoint.health.group.application.status.order" property
- *   in the "application.properties" file so that NO_RESTAURANTS
- *   gets displayed as top-level status for the "application" health group.
- * - Restart the application and verify the result.
- */
